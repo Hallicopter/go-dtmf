c9eed20974352c357f4924323667598534f08e02
hallicopter <advait.raykar@gmail.com>
2021-09-03 14:29:14 +0530

    feat: moves to better implementation of goertzer algorithm from go-dsp

---
+++ utils/dsp/downsample.go
@@ -0,0 +1,92 @@
+package dsp
+
+type LowPassDownsampleComplexFilter struct {
+	Downsample int
+
+	now       complex64
+	prevIndex int
+}
+
+func (fi *LowPassDownsampleComplexFilter) Filter(samples []complex64) []complex64 {
+	return lowPassDownsampleComplexFilterAsm(fi, samples)
+}
+
+func lowPassDownsampleComplexFilterAsm(fi *LowPassDownsampleComplexFilter, samples []complex64) []complex64 {
+	i2 := 0
+	// outputScale := 1.0 / float32(fi.Downsample)
+	for _, v := range samples {
+		fi.now += v
+		fi.prevIndex++
+		if fi.prevIndex < fi.Downsample {
+			continue
+		}
+		samples[i2] = fi.now // complex(real(fi.now)*outputScale, imag(fi.now)*outputScale)
+		fi.prevIndex = 0
+		fi.now = 0
+		i2++
+	}
+	return samples[:i2]
+}
+
+func lowPassDownsampleComplexFilter(fi *LowPassDownsampleComplexFilter, samples []complex64) []complex64 {
+	i2 := 0
+	// outputScale := 1.0 / float32(fi.Downsample)
+	for _, v := range samples {
+		fi.now += v
+		fi.prevIndex++
+		if fi.prevIndex < fi.Downsample {
+			continue
+		}
+		samples[i2] = fi.now // complex(real(fi.now)*outputScale, imag(fi.now)*outputScale)
+		fi.prevIndex = 0
+		fi.now = 0
+		i2++
+	}
+	return samples[:i2]
+}
+
+type LowPassDownsampleRationalFilter struct {
+	Fast, Slow int
+
+	sum       float32
+	prevIndex int
+}
+
+func (fi *LowPassDownsampleRationalFilter) Filter(samples []float32) []float32 {
+	return lowPassDownsampleRationalFilterAsm(fi, samples)
+}
+
+func lowPassDownsampleRationalFilterAsm(fi *LowPassDownsampleRationalFilter, samples []float32) []float32 {
+	i2 := 0
+	fastSlowRatio := float32(fi.Slow) / float32(fi.Fast)
+	for _, v := range samples {
+		fi.sum += v
+		fi.prevIndex += fi.Slow
+		if fi.prevIndex < fi.Fast {
+			continue
+		}
+		samples[i2] = fi.sum * fastSlowRatio
+		i2++
+		fi.prevIndex -= fi.Fast
+		fi.sum = 0.0
+	}
+	return samples[:i2]
+}
+
+
+func lowPassDownsampleRationalFilter(fi *LowPassDownsampleRationalFilter, samples []float32) []float32 {
+	i2 := 0
+	fastSlowRatio := float32(fi.Slow) / float32(fi.Fast)
+	for _, v := range samples {
+		fi.sum += v
+		fi.prevIndex += fi.Slow
+		if fi.prevIndex < fi.Fast {
+			continue
+		}
+		samples[i2] = fi.sum * fastSlowRatio
+		i2++
+		fi.prevIndex -= fi.Fast
+		fi.sum = 0.0
+	}
+	return samples[:i2]
+}
---
+++ utils/dsp/math32.go
@@ -0,0 +1,249 @@
+package dsp
+
+import (
+	"math"
+	"unsafe"
+)
+
+// VMulC64xF32 multiplies a vector of complex values with a vector with real values.
+// This is useful for applying a window to complex samples.
+//
+//     output[i] = complex(real(input[i])*mul[i], imag(input[i])*mul[i])
+func VMulC64xF32(input, output []complex64, mul []float32){
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	if len(mul) < n {
+		n = len(mul)
+	}
+	for i, v := range input[:n] {
+		w := mul[i]
+		output[i] = complex(real(v)*w, imag(v)*w)
+	}
+}
+func vMulC64xF32(input, output []complex64, mul []float32) {
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	if len(mul) < n {
+		n = len(mul)
+	}
+	for i, v := range input[:n] {
+		w := mul[i]
+		output[i] = complex(real(v)*w, imag(v)*w)
+	}
+}
+
+// VMulC64 multiplies eache value of the input by the matching value in the multiplier.
+//
+//     output[i] = input[i] * mul[i]
+func VMulC64(input, output, mul []complex64) {
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	if len(mul) < n {
+		n = len(mul)
+	}
+	for i, v := range input[:n] {
+		output[i] = v * mul[i]
+	}
+}
+
+func VAddF32(input, output []float32) {
+	for i, v := range input {
+		output[i] += v
+	}
+}
+
+func VAddC64(input, output []complex64) {
+	for i, v := range input {
+		output[i] += v
+	}
+}
+
+func VScaleC64(input, output []complex64, scale float32) {
+	in := (*[2 << 25]float32)(unsafe.Pointer(&input[0]))[:len(input)*2]
+	out := (*[2 << 25]float32)(unsafe.Pointer(&output[0]))[:len(output)*2]
+	VScaleF32(in, out, scale)
+}
+
+func VScaleF32(input, output []float32, scale float32){
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	for i, v := range input[:n] {
+		output[i] = v * scale
+	}
+}
+func vscaleF32(input, output []float32, scale float32) {
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	for i, v := range input[:n] {
+		output[i] = v * scale
+	}
+}
+
+func VAbsC64(input []complex64, output []float32) {
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	for i, v := range input[:n] {
+		output[i] = float32(math.Sqrt(float64(real(v)*real(v) + imag(v)*imag(v))))
+	}
+}
+func vAbsC64(input []complex64, output []float32) {
+	n := len(input)
+	if len(output) < n {
+		n = len(output)
+	}
+	for i, v := range input[:n] {
+		output[i] = float32(math.Sqrt(float64(real(v)*real(v) + imag(v)*imag(v))))
+	}
+}
+
+func VMaxF32(input []float32) float32 {
+	max := float32(math.Inf(-1))
+	for _, v := range input {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+func vMaxF32(input []float32) float32 {
+	max := float32(math.Inf(-1))
+	for _, v := range input {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func Conj32(x complex64) complex64    { return complex(real(x), -imag(x)) }
+func FastPhase32(x complex64) float32 { return FastAtan2(imag(x), real(x)) }
+func Phase32(x complex64) float32     { return float32(math.Atan2(float64(imag(x)), float64(real(x)))) }
+
+const (
+	pi2  = math.Pi / 2
+	pi4  = math.Pi / 4
+	pi34 = math.Pi * 3 / 4
+)
+
+// max |error| < 0.01
+func FastAtan2(y, x float32) float32 {
+	absY := y
+	if absY < 0 {
+		absY = -absY
+	}
+	absY += 1e-20 // kludge to prevent 0/0 condition
+	var angle float32
+	if x < 0.0 {
+		r := (x + absY) / (absY - x)
+		angle = pi34 + (0.1963*r*r-0.9817)*r
+	} else if x > 0.0 {
+		r := (x - absY) / (x + absY)
+		angle = pi4 + (0.1963*r*r-0.9817)*r
+	} else if y < 0.0 {
+		return -pi2
+	} else if y > 0.0 {
+		return pi2
+	} else {
+		return 0.0
+	}
+	if y < 0.0 {
+		return -angle // negate if in quad III or IV
+	}
+	return angle
+}
+func fastAtan2(y, x float32) float32 {
+	absY := y
+	if absY < 0 {
+		absY = -absY
+	}
+	absY += 1e-20 // kludge to prevent 0/0 condition
+	var angle float32
+	if x < 0.0 {
+		r := (x + absY) / (absY - x)
+		angle = pi34 + (0.1963*r*r-0.9817)*r
+	} else if x > 0.0 {
+		r := (x - absY) / (x + absY)
+		angle = pi4 + (0.1963*r*r-0.9817)*r
+	} else if y < 0.0 {
+		return -pi2
+	} else if y > 0.0 {
+		return pi2
+	} else {
+		return 0.0
+	}
+	if y < 0.0 {
+		return -angle // negate if in quad III or IV
+	}
+	return angle
+}
+
+// |error| < 0.005
+func FastAtan2_2(y, x float32) float32 {
+	if x == 0.0 {
+		switch {
+		case y > 0.0:
+			return pi2
+		case y < 0.0:
+			return -pi2
+		}
+		return 0.0
+	}
+	z := y / x
+	zz := z * z
+	if zz < 1.0 {
+		atan := z / (1.0 + 0.28*zz)
+		if x < 0.0 {
+			if y < 0.0 {
+				return atan - math.Pi
+			}
+			return atan + math.Pi
+		}
+		return atan
+	}
+	atan := pi2 - z/(zz+0.28)
+	if y < 0.0 {
+		return atan - math.Pi
+	}
+	return atan
+}
+
+func fastAtan2_2(y, x float32) float32 {
+	if x == 0.0 {
+		switch {
+		case y > 0.0:
+			return pi2
+		case y < 0.0:
+			return -pi2
+		}
+		return 0.0
+	}
+	z := y / x
+	zz := z * z
+	if zz < 1.0 {
+		atan := z / (1.0 + 0.28*zz)
+		if x < 0.0 {
+			if y < 0.0 {
+				return atan - math.Pi
+			}
+			return atan + math.Pi
+		}
+		return atan
+	}
+	atan := pi2 - z/(zz+0.28)
+	if y < 0.0 {
+		return atan - math.Pi
+	}
+	return atan
+}
